a55aa2306165c9a186026ff6784ca35deb933d2a
d-sebastian <dinu_sebastian97@icloud.com>
2025-02-18 19:40:16 +0200

    modified ctrl mod tap and added lgui

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,13 +5,13 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SEMI,          \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp LSHIFT,        &mo U_SYM,         &kp RET,           U_MT(LCTRL, BSPC), &kp SPACE,         &mo U_NAV,         U_NP,              U_NP
+U_NP,              U_NP,              U_MT(LSHIFT, TAB), &mo U_SYM,         U_MT(LCTRL, RET),  &kp BSPC,          &kp SPACE,         &mo U_NAV,         U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp EXCL,          &kp AT,            &kp HASH,          &kp DOLLAR,        &kp PERCENT,       &kp CARET,         &kp AMPERSAND,     &kp LPAR,          &kp RPAR,          &kp STAR,          \
 &kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp MINUS,         &kp KP_EQUAL,      &kp LBKT,          &kp RBKT,          &kp APOS,          \
 &kp N6,            &kp N7,            &kp N8,            &kp N9,            &kp N0,            &kp UNDER,         &kp KP_PLUS,       &kp LBRC,          &kp RBRC,          &kp DQT,           \
-U_NP,              U_NP,              &kp LSHIFT,        &mo U_SYM,         &kp RET,           &kp TAB,           &kp ESC,           &kp LALT,              U_NP,              U_NP
+U_NP,              U_NP,              &kp LSHIFT,        &mo U_SYM,         &kp RET,           &kp LGUI,           &kp ESC,           &kp LALT,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
 &kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp F5,            &kp F6,            &kp F7,            &kp F8,            &kp F9,            &kp F10,           \
