261e4248591ff26faee21c765670af5cc10fe4cd
d-sebastian <dinu_sebastian97@icloud.com>
2025-02-18 15:47:01 +0200

    fixed??

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,16 +5,16 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SEMI,          \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp LSHIFT,        &u_mo_U_SYM,       &kp RET,           &kp LCTRL,         &kp SPACE,         &u_mo_U_NAV,       U_NP,              U_NP
+U_NP,              U_NP,              &kp LSHIFT,        &u_to_U_SYM,       &kp RET,           &kp LCTRL,         &kp SPACE,         &u_to_U_NAV,       U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp EXCL,          &kp AT,            &kp HASH,          &kp DOLLAR,        &kp PERCENT,       &kp CARET,         &kp AMPERSAND,     &kp LPAR,          &kp RPAR,          &kp STAR,          \
 &kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp MINUS,         &kp KP_EQUAL,      &kp LBKT,          &kp RBKT,          &kp APOS,          \
 &kp N6,            &kp N7,            &kp N8,            &kp N9,            &kp N0,            &kp UNDER,         &kp KP_PLUS,       &kp LBRC,          &kp RBRC,          &kp DQT,           \
-U_NP,              U_NP,              &kp LSHIFT,        &u_mo_U_SYM,       &kp RET,           &kp TAB,           &kp ESC,           &kp LALT,              U_NP,              U_NP
+U_NP,              U_NP,              &kp LSHIFT,        &u_to_U_SYM,       &kp RET,           &kp TAB,           &kp ESC,           &kp LALT,              U_NP,              U_NP
 
-#define MIRYOKU_LAYER_NAV \
-&kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp F5,            &kp F6,            &kp F7,            &kp F8,            &kp F9,            &kp F10,           \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp ESC,           &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &kp GRAVE,         \
-&u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_clr,         U_NA,              U_RDO,             U_PST,             &kp PIPE,          &kp BLSH,          &kp TILDE,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              &kp SPACE,         &u_mo_U_NAV,             U_NP,              U_NP
+//#define MIRYOKU_LAYER_NAV \
+//&kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp F5,            &kp F6,            &kp F7,            &kp F8,            &kp F9,            &kp F10,           \
+//&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp ESC,           &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &kp GRAVE,         \
+//&u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_clr,         U_NA,              U_RDO,             U_PST,             &kp PIPE,          &kp BLSH,          &kp TILDE,         \
+//U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              &kp SPACE,         &u_mo_U_NAV,             U_NP,              U_NP
